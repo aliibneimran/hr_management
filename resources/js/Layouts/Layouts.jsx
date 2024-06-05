227c0e9aebbd234ad9421b235478c3c1484ed563
Ali Ibne Imran <aliibneimran1996@gmail.com>
2024-06-05 18:47:29 +0600

    add

--- resources/js/Layouts/Layouts.jsx
+++ resources/js/Layouts/Layouts.jsx
@@ -3,8 +3,22 @@ import Footer from '@/Components/Footer';
 import Header from '@/Components/Header';
 import Leftbar from '@/Components/Leftbar';
 import TopLeft from '@/Components/TopLeft';
+import { useEffect } from 'react';
 
 export default function Layouts({children}) {
+    useEffect(() => {
+        // Check if the reload flag is set in localStorage
+        if (!localStorage.getItem("reloaded")) {
+            // Set the flag in localStorage
+            localStorage.setItem("reloaded", "true");
+
+            // Reload the page
+            window.location.reload();
+        } else {
+            // Remove the flag so that the reload can happen again in the future if needed
+            localStorage.removeItem("reloaded");
+        }
+    }, []);
   return (
 <div id="main-wrapper">
     <TopLeft></TopLeft>
@@ -13,6 +27,11 @@ export default function Layouts({children}) {
     <Leftbar></Leftbar>
     <div className="content-body">
         <div className="container-fluid">
+            <div id="preloader">
+                <div>
+                    <img src="assets/images/gif.gif" alt />
+                </div>
+            </div>
             {children}
         </div>
     </div>
